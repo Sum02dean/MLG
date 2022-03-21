0e4825aba0c29cf6d0bf5347b8588a6d39166673
Liine Kasak <liine.kasak@gmail.com>
2022-03-21 15:05:10 +0200

    #17 add cell line info to train dataframe, remove chr prefix from entries

--- task_1/utils/data_loader.py
+++ task_1/utils/data_loader.py
@@ -2,34 +2,56 @@ import os
 
 import pandas as pd
 
-TRAIN_INFO = ['X1_train_info', 'X1_val_info', 'X2_train_info', 'X2_val_info']
-TRAIN_LABELS = ['X1_train_y', 'X1_val_y', 'X2_train_y', 'X2_val_y']
+πTRAIN_INFO = {1: ['X1_train_info', 'X1_val_info'], 2: ['X2_train_info', 'X2_val_info']}
+TRAIN_LABELS = {1: ['X1_train_y', 'X1_val_y'], 2: ['X2_train_y', 'X2_val_y']}
 
 
 def load_info(filename: str) -> pd.DataFrame:
     return pd.read_csv(f'data{os.sep}CAGE-train{os.sep}{filename}.tsv', sep='\t')
 
 
-def load_train_genes() -> pd.DataFrame:
+def load_train_genes_for_cell_line(cell_line: int) -> pd.DataFrame:
     """
-    Load the entire train dataset (with expression values).
+    Load the train dataset (with expression values) for given cell line.
 
+    :param cell_line: 1 or 2 (corresponds to X1 and X2 datasets)
     :return: DataFrame of gene info and y values as 'gex'
     """
-    info_dfs = [load_info(file) for file in TRAIN_INFO]
-    gene_info = pd.concat(info_dfs)
-
-    label_dfs = [load_info(file) for file in TRAIN_LABELS]
-    gene_exp = pd.concat(label_dfs)
+    gene_info = pd.concat([load_info(file) for file in TRAIN_INFO[cell_line]])
+    gene_exp = pd.concat([load_info(file) for file in TRAIN_LABELS[cell_line]])
 
     # verify that the order of genes matches before adding column for expression
     assert (gene_info.gene_name == gene_exp.gene_name).all()
     gene_info['gex'] = gene_exp.gex
 
+    # remove chr prefix from entries
+    gene_info['chr'] = gene_info['chr'].map(lambda x: x.lstrip('chr'))
+
     return gene_info
 
 
+def load_train_genes() -> pd.DataFrame:
+    """
+    Load the entire train dataset (with expression values).
+    Added value of cell gene cell line (1 or 2).
+
+    :return: DataFrame of gene info, cell line number, and y values as 'gex'
+    """
+    gene_dfs = []
+    for cell_line in [1, 2]:
+        df = load_train_genes_for_cell_line(cell_line)
+        df['cell_line'] = cell_line
+        gene_dfs.append(df)
+
+    return pd.concat(gene_dfs)
+
+
 def load_test_genes() -> pd.DataFrame:
+    """
+    Load the test dataset.
+
+    :return: Dataframe of gene info for cell line 3.
+    """
     return load_info('X3_test_info')
 
 
