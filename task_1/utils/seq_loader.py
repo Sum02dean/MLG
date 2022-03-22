2250568bdc121e0119c54e0c33679dd4da534560
Liine Kasak <liine.kasak@gmail.com>
2022-03-22 10:56:43 +0200

    #23 added documentation, and new fun `get_encoded_sequence`

--- task_1/utils/seq_loader.py
+++ task_1/utils/seq_loader.py
@@ -18,6 +18,14 @@ SEQ_TO_ONEHOT = {
 
 
 def get_sequence(chromosome_nr: int, start: int, stop: int) -> str:
+    """
+    Returns sequence as string in given chromosome with positions [start, stop].
+
+    :param chromosome_nr: chromosome from which to read sequence
+    :param start: first nucleotide position to read
+    :param stop: last nucleotide position to read (includes stop!)
+    :return: sequence as a string
+    """
     return GENOME.sequence({'chr': f'chr{chromosome_nr}', 'start': start, 'stop': stop})
 
 
@@ -43,6 +51,18 @@ def encode_seq(seq: str) -> np.ndarray:
     """
     seq = clean_seq(seq)
     return np.array([SEQ_TO_ONEHOT[nucleotide] for nucleotide in seq])
+
+
+def get_encoded_sequence(chromosome_nr: int, start: int, stop: int) -> np.ndarray:
+    """
+    Returns one-hot encoded sequence in given chromosome with positions [start, stop].
+
+    :param chromosome_nr: chromosome from which to read sequence
+    :param start: first nucleotide position to read
+    :param stop: last nucleotide position to read (includes stop!)
+    :return: One-hot encoded sequence as numpy array
+    """
+    return encode_seq(get_sequence(chromosome_nr, start, stop))
 
 
 if __name__ == '__main__':
