b8e1be7b8c32069c93c14c1b64035ba76dfbe53f
sum02dean <sum02dean@gmail.com>
2022-03-24 18:43:29 +0100

    brgan work on rf regressor

---
+++ task_1/utils/rf.py
@@ -0,0 +1,25 @@
+import numpy as np
+from scipy.stats import randint
+from sklearn.ensemble import RandomForestRegressor
+from data_loader import *
+from dataset import *
+import torch
+from histone_loader import*
+from stratification import *
+
+clf = RandomForestRegressor(n_estimators=10)
+
+param_dist = {
+    "max_depth": [3, None],
+    "bootstrap": [True, False],
+    "criterion": ["gini", "entropy"]
+}
+
+train_genes, test_genes = random_splits()
+
+train_dataloader = torch.utils.data.DataLoader(
+    SeqHistDataset(train_genes), shuffle=True, batch_size=16)
+
+for gene_features, gex in tqdm(train_dataloader):
+    print(gene_features.shape, gex.shape)
+    break
